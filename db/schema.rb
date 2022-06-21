356f595bbddfbf04a933336089ce3d99ff065b76
Joshua Boyd <boyd.jc.github@gmail.com>
2022-06-21 10:34:13 -0400

    added placeholders for user and trend posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_210405) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_21_135226) do
   create_table "posts", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
